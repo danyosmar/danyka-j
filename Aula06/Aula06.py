7153ed44e9583c6892f934d9742ec2811dcb6e90
danyosmar <danykajr@bol.com.br>
2026-03-13 21:56:05 -0300

    add

---
+++ Aula06/Aula06.py
@@ -0,0 +1,24 @@
+#lista#
+#ídice:   0           1     2         3       4
+nome = ['matheus,''alice','caio','larissa','miguel']
+print(nome[3 ])
+
+
+# LISTAS:
+
+nomes = ['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+
+nome1 = nomes[0]
+
+print(nomes[-1])
+print(nomes[-2])
+print(nomes[-3])
+print(nomes[-4])
+print(nome1)
+
+primeira_parte=nomes[0:3]
+print(primeira_parte)
+segunda_parte=nomes[3:6]
+print(segunda_parte)
+
+print(len(nomes))
